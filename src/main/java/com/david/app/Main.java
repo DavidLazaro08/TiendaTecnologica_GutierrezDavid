6c3a11891a36cb598acc11b0e81225cff92dd93e
David Gutiérrez <empirio14@alumnos.ilerna.com>
2025-10-20 19:51:58 +0200

    Prueba de lectura Main

--- src/main/java/com/david/app/Main.java
+++ src/main/java/com/david/app/Main.java
@@ -1,7 +1,16 @@
 package com.david.app;
 
+import io.LectorJSON;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.println("PRUEBA DE LECTURA JSON\n");
+
+        String ruta = "src/main/resources/tienda.json";
+
+        LectorJSON lector = new LectorJSON();
+
+        lector.leerJSON(ruta);
     }
 }
